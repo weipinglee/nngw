3b2beb6bb988c131aead5dd5cf00343fee64d068
weipinglee <weiping.lee@163.com>
2016-07-19 14:43:04 +0800

    保存

--- wuhu.php
+++ wuhu.php
@@ -13,90 +13,84 @@
  */
 define('IN_DOUCO', true);
 
+if (isset($_REQUEST['mobile'])) setcookie('client', 'pc'); // 判断时候强制在移动端中显示PC版
+
 require (dirname(__FILE__) . '/include/init.php');
+//var_dump($GLOBALS['_LANG']);exit;
 
-// 验证并获取合法的ID，如果不合法将其设定为-1
-$cat_id = $firewall->get_legal_id('article_category', $_REQUEST['id'], $_REQUEST['unique_id']);
-if ($cat_id == -1) {
-     header('location:'.ROOT_URL);
-} else {
-    $where = ' WHERE cat_id IN (' . $cat_id . $dou->dou_child_id('article_category', $cat_id) . ')';
-}
-    
-// 获取分页信息
-$page = $check->is_number($_REQUEST['page']) ? trim($_REQUEST['page']) : 1;
-$limit = $dou->pager('article', ($_DISPLAY['article'] ? $_DISPLAY['article'] : 10), $page, $dou->rewrite_url('article_category', $cat_id), $where);
-
-/* 获取文章列表 */
-$sql = "SELECT id, title, content, image, cat_id, add_time, click, description FROM " . $dou->table('article') . $where . " ORDER BY id DESC" . $limit;
-$query = $dou->query($sql);
-
-while ($row = $dou->fetch_array($query)) {
-    $url = $dou->rewrite_url('article', $row['id']);
-    $add_time = date("Y-m-d", $row['add_time']);
-    $add_time_short = date("m-d", $row['add_time']);
-    $image = $row['image'] ? ROOT_URL . $row['image'] : '';
-    
-    // 如果描述不存在则自动从详细介绍中截取
-    $description = $row['description'] ? $row['description'] : $dou->dou_substr($row['content'], 200);
-    
-    $article_list[] = array (
-            "id" => $row['id'],
-            "cat_id" => $row['cat_id'],
-            "title" => $row['title'],
-            "image" => $image,
-            "add_time" => $add_time,
-            "add_time_short" => $add_time_short,
-            "click" => $row['click'],
-            "description" => $description,
-            "url" => $url 
-    );
+// 如果存在搜索词则转入搜索页面
+if ($_REQUEST['s']) {
+    if ($check->is_text($keyword = trim($_REQUEST['s']))) {
+        require (ROOT_PATH . 'include/search.inc.php');
+    } else {
+        $dou->dou_msg($_LANG['search_keyword_wrong']);
+    }
 }
 
-// 获取分类信息
-$sql = "SELECT cat_id, cat_name, parent_id FROM " . $dou->table('article_category') . " WHERE cat_id = '$cat_id'";
+// 获取关于我们信息
+$sql = "SELECT * FROM " . $dou->table('page') . " WHERE id = '1'";
 $query = $dou->query($sql);
-$cate_info = $dou->fetch_array($query);
+$about = $dou->fetch_array($query);
+
+// 写入到index数组
+$index['about_name'] = $about['page_name'];
+$index['about_content'] = $dou->dou_substr($about['content'], 300);
+$index['about_link'] = $dou->rewrite_url('page', '1');
+$index['cur'] = true;
 
 // 赋值给模板-meta和title信息
-$smarty->assign('page_title', $dou->page_title('article_category', $cat_id));
-$smarty->assign('keywords', $cate_info['keywords']);
-$smarty->assign('description', $cate_info['description']);
+$smarty->assign('page_title', $dou->page_title());
+$smarty->assign('keywords', $_CFG['site_keywords']);
+$smarty->assign('description', $_CFG['site_description']);
 
 // 赋值给模板-导航栏
 $smarty->assign('nav_top_list', $dou->get_nav('top'));
-$smarty->assign('nav_middle_list', $dou->get_nav('middle', '0', 'article_category', $cat_id, $cate_info['parent_id']));
+$smarty->assign('nav_middle_list', $dou->get_nav('middle'));
+//var_dump($dou->get_nav('middle'));exit;
 $smarty->assign('nav_bottom_list', $dou->get_nav('bottom'));
 
 // 赋值给模板-数据
-$smarty->assign('ur_here', $dou->ur_here('article_category', $cat_id));
-$smarty->assign('cate_info', $cate_info);
-$smarty->assign('article_category', $dou->get_category('article_category', 0, $cat_id));
-$smarty->assign('article_list', $article_list);
+$smarty->assign('show_list', $dou->get_show_list());
+$smarty->assign('link', get_link_list());
 
-$pageBar = getPageBar($smarty->_tpl_vars['pager']);
+$smarty->assign('recommend_product', $dou->get_list('product', 'ALL', $_DISPLAY['home_product'], 'sort DESC'));
+// $smarty->assign('recommend_article', $dou->get_list('article', 'ALL', $_DISPLAY['home_article'], 'sort DESC'));
+$sql = 'select cat_id, cat_name, unique_id from dou_article_category';
+$query = $dou->query($sql);
+while( $tmp = $dou->fetch_assoc($query)){
+    $tmp['url'] = $dou->rewrite_url('article_category', $tmp['cat_id']);
+    $cate[] = $tmp;
+}
+foreach($cate as $key => $val){
+    $cate[$val['unique_id']]['name'] = $val['cat_name'];
+    $cate[$val['unique_id']]['url'] = $val['url'];
+    $cate[$val['unique_id']]['article'] = $dou->get_list('article', $val['cat_id'], $_DISPLAY['home_article'], 'sort DESC'); 
+    unset($cate[$key]);
+}
+// print_r($dou->get_list('article', 'ALL', $_DISPLAY['home_article'], 'sort DESC'));exit;
 
 
-$smarty->assign('pageBar', $pageBar);
-$smarty->display('article_category.dwt');
+$smarty->assign('recommend_article', $cate);
 
-function getPageBar( $pager ){
-    $href = substr($pager['last'], 0, strrpos($pager['last'], 'page=')) . 'page='; //页面链接
-    
-    $curr = $pager['page']; //当前页码
+$smarty->display('wuhu.dwt');
 
-    $count = $pager['page_count']; //总页数
-
-    $left = max($curr-2, 1); //初步计算最左边页码
-
-    $right = min($left + 4, $count); //计算最右边页码
-    $left = max($right-4, 1);// 计算最终左边的页码
-    $pageBar = array();
-    for($i=$left; $i<=$right; $i++){
-        $pageBar[$i]['code'] = $i;
-        $pageBar[$i]['link'] = $href.$i;
+/**
+ * +----------------------------------------------------------
+ * 获取下级幻灯列表
+ * +----------------------------------------------------------
+ */
+function get_link_list() {
+    $sql = "SELECT * FROM " . $GLOBALS['dou']->table('link') . " ORDER BY sort ASC, id ASC";
+    $query = $GLOBALS['dou']->query($sql);
+    while ($row = $GLOBALS['dou']->fetch_array($query)) {
+        $link_list[] = array (
+                "id" => $row['id'],
+                "link_name" => $row['link_name'],
+                "link_url" => $row['link_url'],
+                "sort" => $row['sort'] 
+        );
     }
-     
-    return $pageBar;
+    
+    return $link_list;
 }
 ?>
